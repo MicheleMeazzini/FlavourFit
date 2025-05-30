54979def29f0c40e89c87082c2e0f6d1e48af24e
SteMiche00 <stefano.micheloni31@gmail.com>
2025-05-30 10:50:33 +0200

    Aggiunta Modelli pe

---
+++ FlavourFitAPI/src/main/java/com/example/demo/model/graph/UserNode.java
@@ -0,0 +1,27 @@
+package com.example.demo.model.graph;
+
+import lombok.Data;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Node("User")
+public class UserNode {
+    @Id
+    private String id;
+    private String name;
+
+    @Relationship(type = "FOLLOWS", direction = Relationship.Direction.OUTGOING)
+    private Set<UserNode> followedUsers = new HashSet<>();
+
+    @Relationship(type = "LIKES", direction = Relationship.Direction.OUTGOING)
+    private Set<RecipeNode> likedRecipes = new HashSet<>();
+
+    @Relationship(type = "CREATED", direction = Relationship.Direction.OUTGOING)
+    private Set<RecipeNode> createdRecipes = new HashSet<>();
+}
