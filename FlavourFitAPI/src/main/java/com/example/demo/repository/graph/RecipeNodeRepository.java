13e1ada54af5f833e290f7ec6795eaec35795282
Michele Meazzini <meazzini92@gmail.com>
2025-06-07 18:22:07 +0200

    Error fix

--- FlavourFitAPI/src/main/java/com/example/demo/repository/graph/RecipeNodeRepository.java
+++ FlavourFitAPI/src/main/java/com/example/demo/repository/graph/RecipeNodeRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository.graph;
 
 import com.example.demo.model.graph.RecipeNode;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
